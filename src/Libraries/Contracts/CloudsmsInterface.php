a662401a157a950d0decca72abb0849b203fac21
GKrishna M <gm@leelam.com>
2016-07-03 14:57:04 +0530

    Added sendXML

--- src/Libraries/Contracts/CloudsmsInterface.php
+++ src/Libraries/Contracts/CloudsmsInterface.php
@@ -1,34 +1,35 @@
 <?php
-namespace Leelam\Cloudsms\Libraries\Contracts;
+    namespace Leelam\Cloudsms\Libraries\Contracts;
 
     /**
      * Main gateway interface for all the SMS communication
      *
      * Interface CloudsmsInterface
      */
-interface CloudsmsInterface {
+    interface CloudsmsInterface
+    {
 
-    /**
-     *  Every Sub/child class Requires number numbers
-     *
-     * @param $mobiles
-     * @return mixed
-     */
-    public function numbers($mobiles);
+        /**
+         *  Every Sub/child class Requires number numbers
+         *
+         * @param $mobiles
+         * @return mixed
+         */
+        public function numbers ( $mobiles );
 
-    /**
-     * Every Sub/Child class Requires Message
-     *
-     * @param $content
-     * @return mixed
-     */
-    public function message ( $content );
-
-    /**
-     * Send/Execute
-     * @param $data
-     * @return mixed
-     * @internal param $data
-     */
-    public function send($data = null);
-}
+        /**
+         * Every Sub/Child class Requires Message
+         *
+         * @param $content
+         * @return mixed
+         */
+        public function message ( $content );
+        //public function dataXML ( $xml );
+        /**
+         * Send/Execute
+         * @param $data
+         * @return mixed
+         * @internal param $data
+         */
+        public function send ( $data = null );
+    }
--- src/Libraries/Gateways/Cloudsms.php
+++ src/Libraries/Gateways/Cloudsms.php
@@ -35,10 +35,40 @@ class Cloudsms extends CloudsmsAbstract implements CloudsmsInterface
         return $this;
     }
 
+    /**
+     *  Array of data
+     *  [
+     *      [
+     *          'message'=>'Custome message 1',
+     *          'mobile'=>'9949990991'
+     *      ],
+     *      [
+     *          'message'=>'Custome message 2',
+     *          'mobile'=>'8008008322'
+     *      ]
+     *  ]
+     * @param $messagesAndMobiles
+     * @return $this
+     *
+     */
+    public function dataXML ( $messagesAndMobiles )
+    {
+        $this->dataXML = $messagesAndMobiles;
+
+        return $this;
+    }
+
+    public function campaign ( $campaign )
+    {
+        $this->campaign = $campaign;
+
+        return $this;
+    }
 
     /**
      * @param $data
      * @return $this
+     * @deprecated 
      */
     public function send ( $data = null )
     {
@@ -75,13 +105,76 @@ class Cloudsms extends CloudsmsAbstract implements CloudsmsInterface
         }
 
         //Print error if any
-        if ( curl_errno ( $this->ch ) )
+        if ( curl_errno ( $this->ch ) ) {
             echo 'error:' . curl_error ( $this->ch );
+        }
 
         curl_close ( $this->ch );
 
         return $output;
+
     }
 
+    /**
+     * @return mixed|string
+     */
+    public function sendXML ()
+    {
+        if ( ! collect ( $this->dataXML )->isEmpty () ) {
+            $xml = '<MESSAGE>';
+            $xml .= '<AUTHKEY>' . $this->authkey . '</AUTHKEY>';
+            $xml .= '<ROUTE>' . $this->route . '</ROUTE>';
+            if ( $this->route == 4 OR $this->route == 'template' ) {
+                $xml .= '<SENDER>' . $this->senderId . '</SENDER>';
+            } else {
+                $xml .= '<SENDER>777777</SENDER>';
+            }
+
+            // Customized sms with their respect mobile number
+            foreach ( $this->dataXML as $textAndTo ) {
+                $xml .= '<SMS TEXT="' . $textAndTo[ 'message' ] . '">';
+                $xml .= '<ADDRESS TO="' . $textAndTo[ 'mobile' ] . '"></ADDRESS>';
+                $xml .= '</SMS>';
+            }
+            $xml .= '</MESSAGE>';
+
+            // init the resource
+            $postData[ 'data' ] = $xml;
+            curl_setopt_array ( $this->ch, [
+                CURLOPT_URL            => $this->url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_POST           => true,
+                CURLOPT_POSTFIELDS     => $postData
+                //,CURLOPT_FOLLOWLOCATION => true
+            ] );
+
+            //Ignore SSL certificate verification
+            curl_setopt ( $this->ch, CURLOPT_SSL_VERIFYHOST, 0 );
+            curl_setopt ( $this->ch, CURLOPT_SSL_VERIFYPEER, 0 );
+
+            //get response
+            // var_dump(curl_getinfo($this->ch));
+            if ( env ( 'CLOUDSMS_ENV' ) == 'production' ) {
+                $output = curl_exec ( $this->ch );
+                //  curl_getinfo ($this->ch);
+                \Log::info ( ' Response ' . $output );
+            } else {
+                $output = 'In order to send cloudsms add CLOUDSMS_ENV=production in .env file';
+                \Log::debug ( $output );
+            }
+
+            //Print error if any
+            if ( curl_errno ( $this->ch ) ) {
+                echo 'error:' . curl_error ( $this->ch );
+            }
+
+            curl_close ( $this->ch );
+
+            return $output;
+
+        }
+
+        return false;
+    }
 
 }
